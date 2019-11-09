7b582874cf912014a50441ceb53f4c77b9d0114d
inamake <hara.inamake1902@gmail.com>
2019-11-09 14:06:05 +0900

    クイックリプライ実装テスト(11/9)

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def diagnosis_question(event):
     for i in question:
         items = [QuickReplyButton(action=MessageAction(label=f"{language}", text=f"{language}")) for language in answer_list]
 
-        messages = TextSendMessage(text=i,
+        messages = TextMessage(text=i,
                                quick_reply=QuickReply(items=items))
 
         line_bot_api.reply_message(event.reply_token, messages=messages)
