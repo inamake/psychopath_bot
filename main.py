bc92b8223d9300a960081fd01883892f04a38bbe
inamake <hara.inamake1902@gmail.com>
2019-11-09 13:23:53 +0900

    クイックリプライ実装テスト(11/9)

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import os
 app = Flask(__name__)
 
 diagnosis_class_count = 0
+diagnosis_question_count = 0
 
 #環境変数取得
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
@@ -50,45 +51,54 @@ def callback():
 
 #クイックリプライ機能の実装（診断）
 @handler.add(MessageEvent, message=TextMessage)
-def diagnosis_message(event):
+
+def diagnosis_question_1(event):
     global diagnosis_class_count
+    global diagnosis_question_count
     answer_list = [1, 2, 3, 4, 5]
 
     items = [QuickReplyButton(action=MessageAction(label=f"{language}", text=f"{language}")) for language in answer_list]
 
-    messages = TextSendMessage(text="診断①(選択肢1〜5で答えてください。)",
-                               quick_reply=QuickReply(items=items))
+    if diagnosis_question_count == 0:
+        messages = TextSendMessage(text="診断①(選択肢1〜5で答えてください。)",
+                                   quick_reply=QuickReply(items=items))
 
-    line_bot_api.reply_message(event.reply_token, messages=messages)
+        line_bot_api.reply_message(event.reply_token, messages=messages)
 
-    diagnosis_class_count = diagnosis_class_count + int(items)
+        diagnosis_class_count = diagnosis_class_count + int(items)
+        diagnosis_question_count += 1
 
-    messages = TextSendMessage(text="診断②(選択肢1〜5で答えてください。)",
-                               quick_reply=QuickReply(items=items))
+    elif diagnosis_question_count == 1:
+        messages = TextSendMessage(text="診断②(選択肢1〜5で答えてください。)",
+                                    quick_reply=QuickReply(items=items))
 
-    line_bot_api.reply_message(event.reply_token, messages=messages)
+        line_bot_api.reply_message(event.reply_token, messages=messages)
 
-    diagnosis_class_count = diagnosis_class_count + int(items)
+        diagnosis_class_count = diagnosis_class_count + int(items)
+        diagnosis_question_count += 1
 
-    messages = TextSendMessage(text="診断③(選択肢1〜5で答えてください。)",
-                               quick_reply=QuickReply(items=items))
+    elif diagnosis_question_count == 2:
+        messages = TextSendMessage(text="診断③(選択肢1〜5で答えてください。)",
+                                   quick_reply=QuickReply(items=items))
 
-    line_bot_api.reply_message(event.reply_token, messages=messages)
+        line_bot_api.reply_message(event.reply_token, messages=messages)
 
-    diagnosis_class_count = diagnosis_class_count + int(items)
+        diagnosis_class_count = diagnosis_class_count + int(items)
+        diagnosis_question_count += 1
 
-    if diagnosis_class_count == 3:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="【診断結果】\nあなたはとても良いです。"))
-
-    elif diagnosis_class_count > 3 and diagnosis_class_count <= 6:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたは普通です。"))
-
-    elif diagnosis_class_count > 6 and diagnosis_class_count <= 14:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたはヤバイです。"))
-
-    elif diagnosis_class_count == 15:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたは超絶ヤバイです。"))
-
+    elif diagnosis_question_count == 3:
+        if diagnosis_class_count == 3:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="【診断結果】\nあなたはとても良いです。"))
+            diagnosis_question_count = 0
+        elif diagnosis_class_count > 3 and diagnosis_class_count <= 6:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたは普通です。"))
+            diagnosis_question_count = 0
+        elif diagnosis_class_count > 6 and diagnosis_class_count <= 14:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたはヤバイです。"))
+            diagnosis_question_count = 0
+        elif diagnosis_class_count == 15:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたは超絶ヤバイです。"))
+            diagnosis_question_count = 0
 
 if __name__ == "__main__":
 #    app.run()
