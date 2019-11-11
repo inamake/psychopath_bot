5280bdb51462643a9a16936a8172570d40f2f067
inamake <hara.inamake1902@gmail.com>
2019-11-11 10:49:55 +0900

    クイックリプライ実装テスト(11/11)

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, LocationMessage,
+    MessageEvent, TextMessage, TextSendMessage, QuickReplyButton, MessageAction, QuickReply,
 )
 import os
 
 app = Flask(__name__)
+
+diagnosis_class_count = 0
+
 
 #環境変数取得
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
@@ -40,11 +43,45 @@ def callback():
     return 'OK'
 
 #おうむ返しする。
+#@handler.add(MessageEvent, message=TextMessage)
+#def handle_message(event):
+#    line_bot_api.reply_message(
+#        event.reply_token,
+#        TextSendMessage(text=event.message.text))
+
+#クイックリプライ機能の実装（診断）
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+def diagnosis_question(event):
+    global diagnosis_class_count
+    answer_list = [1, 2, 3, 4, 5]
+    question = ["診断①(選択肢1〜5で答えてください。)","診断②(選択肢1〜5で答えてください。)","診断③(選択肢1〜5で答えてください。)"]
+
+    for i in range(3):
+        items = [QuickReplyButton(action=MessageAction(label=f"{language}", text=f"{language}")) for language in answer_list]
+
+        messages = TextSendMessage(text=question[i],
+                               quick_reply=QuickReply(items=items))
+
+        line_bot_api.reply_message(event.reply_token, messages=messages)
+
+        diagnosis_class_count = diagnosis_class_count + int(items)
+
+# 判定
+#    if diagnosis_class_count == 3:
+#        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="【診断結果】\nあなたはとても良いです。"))
+#
+#    elif diagnosis_class_count > 3 and diagnosis_class_count <= 6:
+#        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたは普通です。"))
+#
+#    elif diagnosis_class_count > 6 and diagnosis_class_count <= 14:
+#        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたはヤバイです。"))
+#
+#    elif diagnosis_class_count == 15:
+#        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="【診断結果】\nあなたは超絶ヤバイです。"))
+#
+#    else:
+#        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="ERROR"))
+
 
 if __name__ == "__main__":
 #    app.run()
