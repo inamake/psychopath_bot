bf301b8a2613ce87c5a2352f6258cf682632f9d1
Frame-wec2 <warframe.wec2@gmail.com>
2019-11-12 12:08:16 +0900

    first

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        # TextSendMessage(text=event.message.text))
-        TextSendMessage(text="https://psychopathbot.herokuapp.com/"))
+        TextSendMessage(text=event.message.text))
+        # TextSendMessage(text="https://psychopathbot.herokuapp.com/"))
 
 
 if __name__ == "__main__":
